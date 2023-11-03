b1267a93287ebe9fa8e0b2acabd5571714d68b27
Chekinm <120395978+Chekinm@users.noreply.github.com>
2023-11-03 23:30:39 +0200

    more tasks

---
+++ tasks/part1/lesson5.py
@@ -0,0 +1,43 @@
+# class Point():
+    
+
+#     def __init__(self, x=0, y=0):
+#         print('hi', )
+#         self.x = x
+#         self.y = y
+#         print('hi2', self)
+
+#     def __str__(self):
+#         return f'Point with coord x={self.x} y={self.y}'
+
+
+#     def set_coord(self, x, y):
+#         self.x = x
+#         self.y = y
+
+#     def __del__(self):
+#         print('deleting instantse of Point', self)
+
+
+# pt = Point(1,2)
+# print(pt)
+# # print('before deletiion', pt)
+# # pt = 5
+# # print('after deletion', pt)
+
+# # task 2
+
+# class Money:
+#     def __init__(self, money=0) -> None:
+#         self.money = money
+
+# task 3
+# class Point:
+#     def __init__(self, x, y, color='black') -> None:
+#         self.x = x
+#         self.y = y
+#         self.color = color
+
+
+# points = [Point(i, i) for i in range(1, 2000, 2)]
+# points[1].color = 'yellow'
