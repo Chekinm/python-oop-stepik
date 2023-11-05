d4c86e66bfaaa357bf89bdb4c5403b9c9c9cef26
Chekinm <120395978+Chekinm@users.noreply.github.com>
2023-11-05 15:40:03 +0200

    task 8

--- tasks/part1/lesson5.py
+++ tasks/part1/lesson5.py
@@ -204,3 +204,68 @@
 # mem2 = Memory('mem2', 20)
 
 # mb = MotherBoard('my_motherboard', cpu, mem1, mem2)
+
+# # task 8
+
+# class Cart:
+#     def __init__(self) -> None:
+#         self.goods = []
+
+#     def add(self, gd):
+#         self.goods.append(gd)
+
+#     def remove(self, indx):
+#         try:
+#             self.goods.pop(indx)
+#         except IndexError:
+#             print('Looks the index is out of range,',
+#                   '\n',
+#                   'Should be greather than 0'
+#                   f'and not less than {len(self.goods)}')
+
+#     def get_list(self):
+#         return [str(gd) for gd in self.goods]
+
+
+# class Goods:
+#     def __init__(self, name, price) -> None:
+#         self.name = name
+#         self.price = price
+    
+#     def __str__(self):
+#         return f'{self.name}: {self.price}'
+
+
+# class Table(Goods):
+#     pass
+
+
+# class TV(Goods):
+#     pass
+
+
+# class Notebook(Goods):
+#     pass
+
+
+# class Cup(Goods):
+#     pass
+
+
+# cart = Cart()
+
+# tv1 = TV("samsung", 1111)
+# tv2 = TV("LG", 1234)
+# table = Table("ikea", 2345)
+# n1 = Notebook("msi", 5433)
+# n2 = Notebook("apple", 542)
+# c = Cup("keepcup", 43)
+
+# cart.add(tv1)
+# cart.add(tv2)
+# cart.add(table)
+# cart.add(n1)
+# cart.add(n2)
+# cart.add(c)
+
+# print(cart.get_list())
