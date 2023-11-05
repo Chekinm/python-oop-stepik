bd84304e9b31ce5893b07d5b55cd1b7bca0e0b2e
Chekinm <120395978+Chekinm@users.noreply.github.com>
2023-11-06 01:33:01 +0200

    task 7

--- tasks/part1/lesson6.py
+++ tasks/part1/lesson6.py
@@ -1,6 +1,26 @@
-# # task 6
+# # # task 6
 
-# class AbstractClass:
+# # class AbstractClass:
+
+# #     def __new__(cls, *arg, **kwarg):
+# #         return f'Ошибка: нельзя создавать объекты абстрактного класса'
+
+# # task 7
+
+# class SingletonFive:
+#     __count = 0
+#     __instance = None
 
 #     def __new__(cls, *arg, **kwarg):
-#         return f'Ошибка: нельзя создавать объекты абстрактного класса'
+#         if cls.__count > 4:
+#             return cls.__instance
+
+#         cls.__instance = super().__new__(cls)
+#         cls.__count += 1
+#         return cls.__instance
+
+#     def __init__(self, name):
+#         self.name = name
+
+
+# objs = [SingletonFive(str(n)) for n in range(10)]
