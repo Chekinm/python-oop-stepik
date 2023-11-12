40f4946b320a73edfefb02ba127433db8253009b
Chekinm <120395978+Chekinm@users.noreply.github.com>
2023-11-13 00:46:19 +0200

    task 10

--- tasks/part2/lesson3.py
+++ tasks/part2/lesson3.py
@@ -211,3 +211,64 @@
 # w = bag.get_total_weight()
 # for t in bag.things:
 #     print(f"{t.name}: {t.weight}")
+
+# task 10
+
+class TVProgram:
+
+    def __init__(self, chanell_name):
+        self.chanell_name = chanell_name
+        self.items = []
+        self.__dict_items = dict()
+
+    def add_telecast(self, telecast):
+        self.__dict_items.setdefault(telecast.uid, telecast)
+        self.items = list(self.__dict_items.values())
+
+    def remove_telecast(self, telecastid):
+        try:
+            self.__dict_items.pop(telecastid, None)
+
+            self.items = list(self.__dict_items.values())
+        except IndexError:
+            print('tlecast doens\'t exist')
+
+
+class Telecast:
+
+    def __init__(self, id, name, duration):
+        self.uid = id
+        self.name = name
+        self.duration = duration
+
+    @property
+    def uid(self):
+        return self.__id
+
+    @uid.setter
+    def uid(self, id):
+        self.__id = id
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
+    @property
+    def duration(self):
+        return self.__duration
+
+    @duration.setter
+    def duration(self, duration):
+        self.__duration = duration
+
+
+pr = TVProgram("Первый канал")
+pr.add_telecast(Telecast(1, "Доброе утро", 10000))
+pr.add_telecast(Telecast(2, "Новости", 2000))
+pr.add_telecast(Telecast(3, "Интервью с Балакиревым", 20))
+for t in pr.items:
+    print(f"{t.name}: {t.duration}")
