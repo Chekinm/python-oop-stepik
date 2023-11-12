71d2164b051d367f879b227ee2bd08343e1c7887
Chekinm <120395978+Chekinm@users.noreply.github.com>
2023-11-13 00:14:06 +0200

    'task8'

--- tasks/part2/lesson3.py
+++ tasks/part2/lesson3.py
@@ -96,3 +96,74 @@
 # print(c.show())
 
 # print(c.login)
+
+# # task 8
+
+# class StringValue:
+
+#     def __init__(self, min_lenght=2, max_lenght=50):
+#         self.__max_lenght = max_lenght
+#         self.__min_lenght = min_lenght
+
+#     def __set_name__(self, owner, name):
+#         self.name = "_" + name
+
+#     def __get__(self, instance, owner):
+#         return getattr(instance, self.name)
+
+#     def __set__(self, instance, value):
+#         if (isinstance(value, str) and
+#                 self.__min_lenght <= len(value) <= self.__max_lenght):
+#             return setattr(instance, self.name, value)
+
+
+# class PriceValue:
+
+#     def __init__(self, max_value=10000):
+#         self.__max_value = max_value
+
+#     def __set_name__(self, owner, name):
+#         self.name = "_" + name
+
+#     def __get__(self, instance, owner):
+#         return getattr(instance, self.name)
+
+#     def __set__(self, instance, value):
+#         if (isinstance(value, int) and 0 <= value <= self.__max_value):
+#             return setattr(instance, self.name, value)
+
+
+# class Product:
+
+#     name = StringValue(min_lenght=2, max_lenght=50)
+#     price = PriceValue(max_value=10000)
+
+#     def __init__(self, name, price):
+#         self.name = name
+#         self.price = price
+
+    
+# class SuperShop:
+
+#     def __init__(self, name):
+#         self.name = name
+#         self.goods = []
+
+#     def add_product(self, product):
+#         self.goods.append(product)
+
+#     def remove_product(self, product):
+#         if product in self.goods:
+#             self.goods.remove(product)
+#         else:
+#             raise ValueError('Cannot delete unexistent product')
+
+
+# shop = SuperShop("У Балакирева")
+# shop.add_product(Product("Курс по Python", 0))
+# shop.add_product(Product("Курс по Python ООП", 2000))
+# for p in shop.goods:
+#     print(f"{p.name}: {p.price}")
+
+
+    
