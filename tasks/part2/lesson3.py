d45e6e0fafbb0a357c9cbb64cf9ac37cbb60ec9d
Chekinm <120395978+Chekinm@users.noreply.github.com>
2023-11-12 23:34:58 +0200

    task 7

--- tasks/part2/lesson3.py
+++ tasks/part2/lesson3.py
@@ -39,3 +39,60 @@
 
 # print(table.cells)
 
+# # task 7
+
+# class ValidateString:
+
+#     def __init__(self, min_length=3, max_length=100):
+#         self.min_lenght = min_length
+#         self.max_lenght = max_length
+
+#     def validate(self, string):
+#         return (isinstance(string, str) and
+#                 self.min_lenght <= len(string) <= self.max_lenght
+#                 )
+
+
+# class StringValue:
+
+#     def __init__(self, validator=ValidateString()):
+#         self.valildator = validator
+
+
+#     def __set_name__(self, owner, name):
+#         self.name = '_' + name
+
+
+#     def __get__(self, instance, owner):
+#         return getattr(instance, self.name)
+    
+#     def __set__(self, instance, value):
+#         if self.valildator.validate(value):
+#             setattr(instance, self.name, value)
+            
+
+# class RegisterForm:
+#     login = StringValue()
+#     password = StringValue()
+#     email = StringValue()
+
+#     def __init__(self, login='login', password ='password', email='email'):
+#         self.login = login
+#         self.password = password
+#         self.email = email
+
+#     def get_fields(self):
+#         return list(self.__dict__.values())
+    
+#     def show(self):
+#         return f'''<form>\nЛогин: {self.login}\nПароль: {self.password}\nEmail: {self.email}\n</form>'''
+        
+
+
+
+# c = RegisterForm()
+
+# print(c.get_fields())
+# print(c.show())
+
+# print(c.login)
