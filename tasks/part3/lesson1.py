b55d5e71af7ea51c007dd77ffc86bf8d02419808
Chekinm <120395978+Chekinm@users.noreply.github.com>
2023-11-14 23:03:46 +0200

    task4complete

--- tasks/part3/lesson1.py
+++ tasks/part3/lesson1.py
@@ -62,5 +62,72 @@
 # book = Book('Python ООП', 'Сергей Балакирев', 123, 2022)
 # book = Book(True, 'Сергей Балакирев', 123, 2022)
 
+# task 4
+from typing import Union
 
+
+class Product:
+    __counter = 0
+
+    def __new__(cls, *args, **kwargs):
+        cls.__counter += 1
+        return super().__new__(cls)
+
+    def __init__(self,
+                 name: str,
+                 weight: Union[int, float],
+                 price: Union[int, float],
+                 id: int = 0
+                 ) -> None:
+        self.id = self.__counter
+        self.name = name
+        self.weight = weight
+        self.price = price
+
+    def __setattr__(self, attr: str, value: any):
+        if ((attr in ('weight', 'price') and
+             not isinstance(value, (float, int))) or
+            (attr == 'name' and
+             not isinstance(value, str))):
+        # if not isinstance(value, self.__init__.__annotations__[attr]):
+            raise TypeError("Неверный тип присваиваемых данных.")
+        super().__setattr__(attr, value)
+
+    def __delattr__(self, attr: str) -> None:
+        if attr == 'id':
+            raise AttributeError("Атрибут id удалять запрещено.")
+        super().__delattr__(self, attr)
+
+
+class Shop:
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.goods = []
+
+    def add_product(self, product: Product) -> None:
+        self.goods.append(product)
+
+    def remove_product(self, product: Product) -> None:
+        self.goods.remove(product)
+
+
+
+
+
+shop = Shop("Балакирев и К")
+book = Product("Python ООП", 100, 1024)
+shop.add_product(book)
+shop.add_product(Product("Python", 150, 512))
+for p in shop.goods:
+    print(f"{p.name}, {p.weight}, {p.price}")
+
+
+print(book.__init__.__annotations__['weight'])
+
+
+
+
+
+    
 
