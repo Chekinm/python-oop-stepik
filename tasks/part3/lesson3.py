b3c3e84af0f421f778a5b55ac419aef338e10bbf
Chekinm <120395978+Chekinm@users.noreply.github.com>
2023-11-16 19:59:08 +0200

    task 4

--- tasks/part3/lesson3.py
+++ tasks/part3/lesson3.py
@@ -20,23 +20,58 @@
 
 # print(b)
 
-# task 3
+# # task 3
 
-class Model:
+# class Model:
 
-    def __init__(self):
-        self.data = {}
+#     def __init__(self):
+#         self.data = {}
 
-    def query(self, **kwargs):
-        self.data = kwargs
+#     def query(self, **kwargs):
+#         self.data = kwargs
 
-    def __str__(self):
-        if self.data:
-            return f'Model: {", ".join(f"{key} = {value}" for key, value in self.data.items())}'
+#     def __str__(self):
+#         if self.data:
+#             return f'Model: {", ".join(f"{key} = {value}" for key, value in self.data.items())}'
 
-        return 'Model'
+#         return 'Model'
 
 
-model = Model()
-model.query(a=1, b=2)
-print(model)
+# model = Model()
+# model.query(a=1, b=2)
+# print(model)
+
+# class WordString:
+
+#     def __init__(self, string=""):
+#         self.__words = []
+#         self.string = string
+
+
+#     @property
+#     def string(self):
+#         return self.__string
+
+#     @string.setter
+#     def string(self, string):
+#         self.__string = string
+#         self.__words = list(string.split())
+
+#     def __len__(self):
+#         return len(self.__words)
+
+#     def __call__(self, indx):
+#         try:
+#             return self.__words[indx]
+#         except IndexError:
+#             print(f'index {indx} is out of range')
+#         except TypeError: 
+#             print(f'index {indx} is not a number')
+        
+# words = WordString()
+# words.string = "Курс по Python ООП"
+# n = len(words)
+# first = "" if n == 0 else words(0)
+# print(words.string)
+# print(f"Число слов: {n}; первое слово: {first}")
+# words(None)
