b6307c253aec74b5142c38786e1b5d0d05851609
Chekinm <120395978+Chekinm@users.noreply.github.com>
2023-11-18 23:32:32 +0200

    task 5

--- tasks/part3/lesson4.py
+++ tasks/part3/lesson4.py
@@ -116,3 +116,107 @@
 # a = NewList([2, 3])
 # res_4 = [1, 2, 2, 3] - a # NewList: [1, 2]
 # assert res_4.get_list() == [1, 2], "метод get_list вернул неверный список"
+
+# task 5
+
+class ListMath:
+
+    def __init__(self, ls=None):
+        if ls is None:
+            self.lst_math = []
+        else:
+            self.lst_math = list(filter(lambda x: type(x) in (int, float) and type(x) is not bool, ls))
+
+    def perform(func):
+        func_name = func.__name__
+        if func_name[2] == 'r':
+            def perform_right(self, other):
+                return ListMath(list(map(lambda x: x.__getattribute__(func_name)(other), self.lst_math)))
+            return perform_right
+
+        elif func_name[2] == 'i':
+
+            def perform_self(self, other):
+                self.list = list(map(lambda x: x.__getattribute__(func_name)(other), self.lst_math))
+            return perform_self
+    
+        def perform_left(self, other):
+            return ListMath(list(map(lambda x: x.__getattribute__(func_name)(other), self.lst_math)))
+        return perform_left
+    
+
+
+
+    @perform
+    def __add__(self, other):
+        pass
+
+    @perform
+    def __radd__(self, other):
+        pass
+
+
+    @perform
+    def __mul__(self, other):
+        pass
+
+    @perform
+    def __rmul__(self, other):
+        pass
+
+    @perform
+    def __truediv__(self, other):
+        pass
+
+    @perform
+    def __rtruediv__(self, other):
+        pass
+
+    @perform
+    def __sub__(self, other):
+        pass
+
+    @perform
+    def __rsub__(self, other):
+        pass
+
+
+
+
+
+        
+
+
+
+
+lst1 = ListMath()
+lp = [1, False, 2, -5, "abc", 7]
+lst2 = ListMath(lp)
+lst3 = ListMath(lp)
+
+assert id(lst2.lst_math) != id(lst3.lst_math), "внутри объектов класса ListMath должна создаваться копия списка"
+
+assert lst1.lst_math == [] and lst2.lst_math == [1, 2, -5, 7], "неверные значения в списке объекта класса ListMath"
+
+res1 = lst2 + 76
+lst = ListMath([1, 2, 3])
+lst += 5
+assert lst.lst_math == [6, 7, 8] and res1.lst_math == [77, 78, 71, 83], "неверные значения, полученные при операциях сложения"
+
+lst = ListMath([0, 1, 2])
+res3 = lst - 76
+res4 = 7 - lst
+assert res3.lst_math == [-76, -75, -74] and res4.lst_math == [7, 6, 5], "неверные значения, полученные при операциях вычитания"
+
+lst -= 3
+assert lst.lst_math == [-3, -2, -1], "неверные значения, полученные при операции вычитания -="
+
+lst = ListMath([1, 2, 3])
+res5 = lst * 5
+res6 = 3 * lst
+lst *= 4
+assert res5.lst_math == [5, 10, 15] and res6.lst_math == [3, 6, 9], "неверные значения, полученные при операциях умножения"
+assert lst.lst_math == [4, 8, 12], "неверные значения, полученные при операциях умножения"
+
+lst = lst / 2
+lst /= 13.0
