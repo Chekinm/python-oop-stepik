ed0798946da3554a701c264556055fe9dab447d5
Chekinm <120395978+Chekinm@users.noreply.github.com>
2023-12-01 01:28:45 +0200

    task 7

--- tasks/part3/lesson4.py
+++ tasks/part3/lesson4.py
@@ -313,3 +313,44 @@
 # #     print(h.data)
 # #     h = h.next
 
+# # task 7
+
+
+# class Book:
+
+#     def __init__(self, title, author, year):
+#         self.title = title
+#         self.author = author
+#         self.year = year
+
+
+# class Lib:
+    
+#     def __init__(self) -> None:
+#         self.book_list = []
+
+    
+#     def __add__(self, other):
+#         self.book_list.append(other)
+#         return self
+    
+#     def __iadd__(self, other):
+#         self.book_list.append(other)
+#         return self
+    
+#     def __sub__(self, other):
+#         if isinstance(other, Book):
+#             if other in self.book_list:
+#                 self.book_list.remove(other)
+#             else:
+#                 raise ValueError
+#         elif type(other) == int:
+#             if other >= 0 and other < len(self.book_list):
+#                 self.book_list.pop(other)
+#         return self
+    
+#     def __isub__(self, other):
+#         return self.__sub__(other)
+
+#     def __len__(self):
+#         return len(self.book_list)
