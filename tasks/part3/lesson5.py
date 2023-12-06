f6467f0d0e486669508b0b89ef4039c5a3867110
Chekinm <120395978+Chekinm@users.noreply.github.com>
2023-12-06 19:57:48 +0200

    task 7

--- tasks/part3/lesson5.py
+++ tasks/part3/lesson5.py
@@ -199,54 +199,75 @@
 # print(lst_text_sorted)
 
 
-# task 6
+# # task 6
 
-class Morph:
+# class Morph:
 
-    def __init__(self, *words):
-        self.words = []
-        for word in words:
-            self.words.append(word.lower())
+#     def __init__(self, *words):
+#         self.words = []
+#         for word in words:
+#             self.words.append(word.lower())
         
         
-    def add_word(self, word):
-        if word.lower() not in self.words:
-            self.words.append(word.lower())
+#     def add_word(self, word):
+#         if word.lower() not in self.words:
+#             self.words.append(word.lower())
 
-    def get_words(self):
-        return tuple(self.words)
+#     def get_words(self):
+#         return tuple(self.words)
 
-    def __eq__(self, other):
-        if isinstance(other, str):
-            return other.lower() in self.words
+#     def __eq__(self, other):
+#         if isinstance(other, str):
+#             return other.lower() in self.words
         
     
 
 
-dict_words = [Morph('связь', 'связи', 'связью', 'связи', 'связей', 'связям', 'связями', 'связях'),
-              Morph('формула', 'формулы', 'формуле', 'формулу', 'формулой', 'формул', 'формулам', 'формулами',
-                    'формулах',
-                    ),
-              Morph('вектор', 'вектора', 'вектору', 'вектором', 'векторе', 'векторы', 'векторов', 'векторам',
-                    'векторами', 'векторах',
-                    ),
-              Morph('эффект', 'эффекта', 'эффекту', 'эффектом', 'эффекте', 'эффекты', 'эффектов', 'эффектам',
-                    'эффектами', 'эффектах'
-                    ), 
-              Morph('день', 'дня', 'дню', 'днем', 'дне', 'дни', 'дням', 'днями', 'днях',
-                    )]
+# dict_words = [Morph('связь', 'связи', 'связью', 'связи', 'связей', 'связям', 'связями', 'связях'),
+#               Morph('формула', 'формулы', 'формуле', 'формулу', 'формулой', 'формул', 'формулам', 'формулами',
+#                     'формулах',
+#                     ),
+#               Morph('вектор', 'вектора', 'вектору', 'вектором', 'векторе', 'векторы', 'векторов', 'векторам',
+#                     'векторами', 'векторах',
+#                     ),
+#               Morph('эффект', 'эффекта', 'эффекту', 'эффектом', 'эффекте', 'эффекты', 'эффектов', 'эффектам',
+#                     'эффектами', 'эффектах'
+#                     ), 
+#               Morph('день', 'дня', 'дню', 'днем', 'дне', 'дни', 'дням', 'днями', 'днях',
+#                     )]
 
-# text = input()
-text = "Мы будем устанавливать связь завтра днем."
-t = text.strip(' .,!?').split()
-print(t)
-count = 0
-for word in t:
-    for mf in dict_words:
-        if word == mf:
-            count += 1
+# # text = input()
+# text = "Мы будем устанавливать связь завтра днем."
+# t = text.strip(' .,!?').split()
+# print(t)
+# count = 0
+# for word in t:
+#     for mf in dict_words:
+#         if word == mf:
+#             count += 1
 
-print(count)
+# print(count)
 
-for mf in dict_words:
-    print(mf.get_words())
+# for mf in dict_words:
+#     print(mf.get_words())
+
+# # task 7 
+
+# class FileAcceptor:
+
+#     def __init__(self, *exts):
+#         self.exts = list(exts)
+
+#     def __call__(self, filename):
+#         ext = filename.split('.')[-1]
+#         return ext in self.exts
+    
+
+#     def __add__(self, other):
+#         res = FileAcceptor(*self.exts)
+#         for ext in other.exts:
+#             if ext not in res.exts:
+#                 res.exts.append(ext)
+#         return res
+                
+    
